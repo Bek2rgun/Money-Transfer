07230d1c558f20b7791b3c656d43e0ffd5ea0d79
West <me@example.com>
2023-05-14 15:44:15 +0600

    Service connected

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from service import transfer
+
 def main():
-    pass
+    user_action = input('Enter 1 to transfer\n Enter 2 to exit\n')
+    while True:
+        if user_action == '1':
+            transfer()
+            user_action = input('Enter 1 to transfer\n Enter 2 to exit\n')
+        elif user_action == '2':
+            print('Goodbye!')
+            break
+        else:
+            print('Incorrect command')
+    
 
 if __name__ == '__main__':
     main()
